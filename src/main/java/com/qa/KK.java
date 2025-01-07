926257dd7f2e187e4a1084813db4466a86263644
Umesh-upadhyay <upadhyayumesh8@gmail.com>
2025-01-07 13:53:58 +0530

    fifth commit

--- src/main/java/com/qa/KK.java
+++ src/main/java/com/qa/KK.java
@@ -1,6 +1,35 @@
 package com.qa;
 
-public class KK {
-	String kk = "the umesh";
+import java.util.List;
 
-}
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class KK {
+//	String kk = "the umesh";
+//	String kks = "the sumesh";
+	 public static void main(String[] args) {
+	WebDriver driver = new EdgeDriver();
+	driver.get("https://www.amazon.in");
+	List<WebElement> Linklist = driver.findElements(By.tagName("a"));
+	int totalinks = Linklist.size();
+	System.out.println(totalinks);
+	
+	int count = 0;
+	
+	for(WebElement e : Linklist) {
+	String Linklist2 = e.getText();
+	String URL = e.getAttribute("href");
+	count ++;
+	System.out.println(Linklist2 + "======" + URL);
+	
+	}
+	
+	}
+	}
+	
+	
+	
+
