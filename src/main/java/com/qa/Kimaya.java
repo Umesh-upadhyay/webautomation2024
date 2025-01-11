2a009d7878e5054fef691451ed4f4a174f8d2b6d
Umesh-upadhyay <upadhyayumesh8@gmail.com>
2025-01-11 12:46:52 +0530

    new with o

--- src/main/java/com/qa/Kimaya.java
+++ src/main/java/com/qa/Kimaya.java
@@ -6,6 +6,6 @@ public class Kimaya {
 	String name = "my name is kimaya";
 	String name2 = "my name is umesh";
 	String name3 = "my name is OM";
-	String name4 = "my name is OM ji sharma";
+	String name4 = "my name is O";
 
 }
